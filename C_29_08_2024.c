b14f480f53f26bb2be986ee97579f1e56dc5c4ef
MarcSmg <dossagaby@gmail.com>
2024-08-31 16:33:25 +0100

    Update C_29_08_2024.c

---
+++ C_29_08_2024.c
@@ -0,0 +1,14 @@
+ #include<string.h>
+  
+char* analyser_age(int age){
+static char message[50];
+if(age<18) {strcpy(message,"Vous êtes jeune.");
+}
+if(18<age<35) {strcpy(message,"Vous êtes un jeune adulte.");
+}
+if(35<age<60) {strcpy(message,"Vous êtes d'âge moyen.");
+}
+if(60<age) {strcpy(message,"Vous êtes vieux.");
+}
+return message;
+}
